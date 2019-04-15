71d6d206015a614665833057945fe9f9678a99c6
Pawel-Gaborek <44684528+Pawel-Gaborek@users.noreply.github.com>
2019-04-16 00:36:59 +0200

    "Aktualizacja kodu nr 3"

--- LABOLATORIUM_NR-3.cpp
+++ LABOLATORIUM_NR-3.cpp
@@ -7,47 +7,66 @@
 //
 
 #include "LABOLATORIUM_NR-3.hpp"
+#include <iostream>
+#include <string>
+#include <sstream>
+
 
 
 //rozpoczynam pisanie kodu dla klasy opisujacej pojedynczego zawodnika
 class PersonalOfPlayer
 {
-    ;
-}
+    char PlayerName;
+    char PlayerSurname;
+    char PlayerHeight;
+    char PlayerPosition;
+    char PlayerDribble;
+    char PlayerTechnic;
+    
+public:
+    
+    
+private:
+    
+};
 //rozpoczynam pisanie kodu dla klasy grupujacej zawodnikow do okreslonej druzyny
 class TeamWithPlayers
 {
-    ;
-}
+public:
+    
+private:
+    
+};
 //rozpoczynam pisanie kodu dla konstruktora kopiujacego zawodnikow druzyny nr 2 do druzyny nr 3
 class ConstructorTeamPlayers
 {
+public:
     
-}
+private:
+    
+};
 
 //rozpoczynam funkcjie glowna
-int main
+int main()
 {
     using namespace std;
     //ponizej definiuje zmienne i ich typy (glownie dla menu kontekstowego dla menu uzytkownika)
     char choice;
-    int exit==0;
+    int exit = 0;
     char continuation;
     int TeamNumbers;
     
     while(exit==0)
     {
-        cout << "Super ! Wlasnie uruchomiles program do labolatorium nr 3 na kierunku Informatyka"
-        cout << "Program umozliwi Ci stworzenie kilku druzyn. Od 2 do ''n''."
-        cout << "Jezeli bedziesz chcial to bedziesz mogl przekopiowac za pomoca konstruktora zawodnikow"
-        cout << "jednej druzyny do drugiej druzyny."
-        cout << "Powodzenia i fajnej zabawy przy tworzeniu druzyny marzen ktora zwycieza w ''Champions Leage'' !"
-        cout << "/n/n/n/n/n/n/n/n/n/n/n/n/n/n"
-        cout << "Podaj liczbe druzyn: n/"
+        cout << "Super ! Wlasnie uruchomiles program do labolatorium nr 3 na kierunku Informatyka\n";
+        cout << "Program umozliwi Ci stworzenie kilku druzyn. Od 2 do ''n''.\n";
+        cout << "Jezeli bedziesz chcial to bedziesz mogl przekopiowac za pomoca konstruktora zawodnikow\n";
+        cout << "jednej druzyny do drugiej druzyny.\n";
+        cout << "Powodzenia i fajnej zabawy przy tworzeniu druzyny marzen ktora zwycieza w ''Champions Leage'' !\n";
+        cout << "\n\n\n\n\n";
+        cout << "Podaj liczbe druzyn: \n";
         
         TeamNumbers = getchar();
-        
-        
         
         while(1)
         {
